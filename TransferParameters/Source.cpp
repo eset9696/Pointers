74bbbed85afaa195e841a24401ddf4db4e3ca085
Mikhail Sherkunkov <sherkunkov@gmail.com>
2023-04-19 20:40:35 +0800

    transfer parameter by value

--- TransferParameters/Source.cpp
+++ TransferParameters/Source.cpp
@@ -1,22 +1,24 @@
 #include <iostream>
 
 using namespace std;
+
 #define delimiter "\n-------------------------------------------------\n"
-void Exchange(int& a, int& b);
+
+void Exchange(int a, int b);
 void main() {
 	setlocale(LC_ALL, "");
 	int a = 2, b = 3;
 	cout << a << "\t" << b << endl;
-	cout << &a << "\t" << &b << endl;
+	cout << "main " << &a << "\t" << &b << endl;
 	cout << delimiter << endl;
 	Exchange(a, b);
 	cout << delimiter << endl;
 	cout << a << "\t" << b << endl;
-	cout << &a << "\t" << &b << endl;
+	cout << "main " << &a << "\t" << &b << endl;
 }
-void Exchange(int& a, int& b) {
+void Exchange(int a, int b) {
 	int buffer = a;
 	a = b;
 	b = buffer;
-	cout << "Exchange" << &a << "\t" << &b << endl;
+	cout << "Exchange " << &a << "\t" << &b << endl;
 }
