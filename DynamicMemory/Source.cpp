bda5a7ecb415a63bccf338dd2c857db35d121532
Mikhail Sherkunkov <sherkunkov@gmail.com>
2023-04-23 15:52:28 +0800

    added functions Push_Back, Push_Front, Insert, Pop_Back, Pop_Front, Erase

--- DynamicMemory/Source.cpp
+++ DynamicMemory/Source.cpp
@@ -5,6 +5,12 @@ using namespace std;
 
 void FillRand(int* arr, const int n);
 void Print(int* arr, const int n);
+void Push_Back(int*& array, int& size, int& value);
+void Push_Front(int*& array, int& size, int& value);
+void Insert(int*& array, int& size, int& value, int& pos);
+void Pop_Back(int*& array, int& size);
+void Pop_Front(int*& array, int& size);
+void Erase(int*& array, int& size, int& pos);
 
 void main(){
 	setlocale(LC_ALL, "");
@@ -19,30 +25,33 @@ void main(){
 	FillRand(arr, n);
 	Print(arr, n);
 	
-	
 	int value;
-	cout << "Введите добавляемое значение" << endl;
+	cout << "Введите добавляемое значение: " << endl;
 	cin >> value;
-	//1) создаем буферный массив нужного размера (на 1 элемент больше)
-	int* buffer = new int[n + 1];
-	//2) копируем все значения из исходного массива в буферный массив
-	for (int i = 0; i < n; i++) {
-		buffer[i] = arr[i];
-	}
-	//3) Удаляем исходный массив
-	delete[] arr;
-	//4) Подменяем адрес исходного массива адресом нового массива
-	arr = buffer;
-	buffer = nullptr; // убираем второй указатель на одну область памяти
-	//5) И только после этого в массив "arr" можно добавить новое значение
-	arr[n] = value;
-	//6) После добавления элемента в массив количество его элементов увеличивается на 1
-	n++;
-	//7) Mission complete - элемент добавлен в массив
+
+	int pos;
+	cout << "Введите номер позиции: " << endl;
+	cin >> pos;
+
+	Push_Back(arr, n, value);
+	Print(arr, n);
+
+	Push_Front(arr, n, value);
+	Print(arr, n);
+
+	Insert(arr, n, value, pos);
+	Print(arr, n);	
+
+	Pop_Back(arr, n);
+	Print(arr, n);
+
+	Erase(arr, n, pos);
+	Print(arr, n);
+
+	Pop_Front(arr, n);
 	Print(arr, n);
 
 	delete[] arr;
-	arr = new int[n + 1];
 }
 void FillRand(int* arr, const int n) {
 	for (int i = 0; i < n; i++)
@@ -62,4 +71,66 @@ void Print(int* arr, const int n) {
 		cout << *p_arr << "\t";
 	}
 	cout << endl;*/
+}
+void Push_Back(int *&array, int& size, int& value) {
+	int* buffer = new int[size + 1];
+	for (int i = 0; i < size; i++) {
+		buffer[i] = array[i];
+	}
+	delete[] array;
+	array = buffer;
+	buffer = nullptr; 
+	array[size] = value;
+	size++;
+}
+void Push_Front(int*& array, int& size, int& value) {
+	size++;
+	int* buffer = new int[size];
+	for (int i = 1; i < size; i++) {
+		buffer[i] = array[i - 1];
+	}
+	delete[] array;
+	array = buffer;
+	buffer = nullptr;
+	array[0] = value;
+}
+void Insert(int*& array, int& size, int& value, int& pos) {
+	size++;
+	int* buffer = new int[size];
+	for (int i = 0, j = 0; i < size; i++, j++) {
+		i == pos ? buffer[i] = value, j-- : buffer[i] = array[j];
+	}
+	delete[] array;
+	array = buffer;
+	buffer = nullptr;
+}
+void Pop_Back(int*& array, int& size) {
+	size--;
+	int* buffer = new int[size];
+	for (int i = 0; i < size; i++) {
+		buffer[i] = array[i];
+	}
+	delete[] array;
+	array = buffer;
+	buffer = nullptr;
+}
+void Pop_Front(int*& array, int& size) {
+	size--;
+	int* buffer = new int[size];
+	for (int i = 1; i < size + 1; i++) {
+		buffer[i - 1] = array[i];
+	}
+	delete[] array;
+	array = buffer;
+	buffer = nullptr;
+}
+void Erase(int*& array, int& size, int& pos) {
+	size--;
+	int* buffer = new int[size];
+	for (int i = 0, j = 0; i < size; i++, j++) {
+		i == pos ? j++, buffer[i] = array[j]: buffer[i] = array[j];
+	}
+	delete[] array;
+	array = buffer;
+	buffer = nullptr;
 }
