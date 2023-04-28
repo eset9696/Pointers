6fab60ec92de96af7a114071d7dfab68954dfff8
Mikhail Sherkunkov <sherkunkov@gmail.com>
2023-04-28 23:01:50 +0800

    dynamic memory 2 demensional arrays

--- DynamicMemory/Source.cpp
+++ DynamicMemory/Source.cpp
@@ -2,12 +2,21 @@
 #include <ctime>
 
 using namespace std;
-
+using std::cin;
+using std::cout;
+using std::endl;
+//#define DYNAMIC_MEMORY_1
+#define DYNAMIC_MEMORY_2
 void FillRand(int* arr, const int n);
+void FillRand(int** arr, const int rows, const int cols);
 void Print(int* arr, const int n);
+void Print(int** arr, const int rows, const int cols);
 void Push_Back(int*& array, int& size, int& value);
+void Push_Back_Rows(int**& array, int& rows, const int cols);
 void Push_Front(int*& array, int& size, int& value);
+void Push_Front_Rows(int**& array, int& rows, const int cols);
 void Insert(int*& array, int& size, int& value, int& pos);
+void Insert_Row(int**& array, int& rows, const int& cols, const int& pos);
 void Pop_Back(int*& array, int& size);
 void Pop_Front(int*& array, int& size);
 void Erase(int*& array, int& size, int& pos);
@@ -15,7 +24,7 @@ void Erase(int*& array, int& size, int& pos);
 void main(){
 	setlocale(LC_ALL, "");
 	srand(time(0));
-
+#ifdef DYNAMIC_MEMORY_1
 	int n = 5;
 
 	cout << "Введите размер массива: " << endl;
@@ -24,7 +33,7 @@ void main(){
 	int* arr = new int[n];
 	FillRand(arr, n);
 	Print(arr, n);
-	
+
 	int value;
 	cout << "Введите добавляемое значение: " << endl;
 	cin >> value;
@@ -40,7 +49,7 @@ void main(){
 	Print(arr, n);
 
 	Insert(arr, n, value, pos);
-	Print(arr, n);	
+	Print(arr, n);
 
 	Pop_Back(arr, n);
 	Print(arr, n);
@@ -52,11 +61,59 @@ void main(){
 	Print(arr, n);
 
 	delete[] arr;
+#endif // DYNAMIC_MEMORY_1
+
+#ifdef DYNAMIC_MEMORY_2
+
+#endif // DYNAMIC_MEMORY_2
+
+
+
+	
+	int rows = 4;
+	int cols = 5;
+	int pos = 3;
+
+	//Создаем двумерный динамический массив
+	int** array = new int*[rows];
+	for (int i = 0; i < rows; i++)
+	{
+		array[i] = new int[cols];
+	}
+
+	FillRand(array, rows, cols);
+	Print(array, rows, cols);
+
+	Push_Back_Rows(array, rows, cols);
+	Print(array, rows, cols);
+
+	Push_Front_Rows(array, rows, cols);
+	Print(array, rows, cols);
+
+	Insert_Row(array, rows, cols, pos);
+	Print(array, rows, cols);
+
+	//Удаляем двумерный динамический массив
+	for (int i = 0; i < rows; i++)
+	{
+		delete[] array[i];
+	}
+	delete[] array;
 }
 void FillRand(int* arr, const int n) {
 	for (int i = 0; i < n; i++)
 	{
 		*(arr + i) = rand() % 100;
+	}
+}
+void FillRand(int** arr, const int rows, const int cols) {
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < cols; j++)
+		{
+			arr[i][j] = rand() % 100;
+		}
+		
 	}
 }
 void Print(int* arr, const int n) {
@@ -72,7 +129,18 @@ void Print(int* arr, const int n) {
 	}
 	cout << endl;*/
 }
-void Push_Back(int *&array, int& size, int& value) {
+void Print(int** arr, const int rows, const int cols) {
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < cols; j++)
+		{
+			cout << arr[i][j] << "\t";
+		}
+		cout << endl;
+	}
+	cout << endl;
+}
+void Push_Back(int*& array, int& size, int& value) {
 	int* buffer = new int[size + 1];
 	for (int i = 0; i < size; i++) {
 		buffer[i] = array[i];
@@ -83,7 +151,33 @@ void Push_Back(int *&array, int& size, int& value) {
 	array[size] = value;
 	size++;
 }
-void Push_Front(int*& array, int& size, int& value) {
+void Push_Back_Rows(int**& array, int& rows, const int cols) {
+	int** buffer = new int*[rows + 1];
+	for (int i = 0; i < rows + 1; i++)
+	{
+		buffer[i] = new int[cols];
+	}
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < cols; j++) {
+			buffer[i][j] = array[i][j];
+		}
+	}
+	for (int i = 0; i < rows; i++)
+	{
+		delete[] array[i];
+	}
+	delete[] array;
+	array = buffer;
+	buffer = nullptr;
+	for (int i = 0; i < cols; i++)
+	{
+		array[rows][i] = 0;
+	}
+	rows++;
+}
+
+void Push_Front(int*& array, int& size, int& value){
 	size++;
 	int* buffer = new int[size];
 	for (int i = 1; i < size; i++) {
@@ -94,6 +188,31 @@ void Push_Front(int*& array, int& size, int& value) {
 	buffer = nullptr;
 	array[0] = value;
 }
+void Push_Front_Rows(int**& array, int& rows, const int cols) {
+	int** buffer = new int* [rows + 1];
+	for (int i = 0; i < rows + 1; i++)
+	{
+		buffer[i] = new int[cols];
+	}
+	for (int i = 1; i < rows + 1; i++)
+	{
+		for (int j = 0; j < cols; j++) {
+			buffer[i][j] = array[i - 1][j];
+		}
+	}
+	for (int i = 0; i < rows; i++)
+	{
+		delete[] array[i];
+	}
+	delete[] array;
+	array = buffer;
+	buffer = nullptr;
+	for (int i = 0; i < cols; i++)
+	{
+		array[0][i] = 0;
+	}
+	rows++;
+}
 void Insert(int*& array, int& size, int& value, int& pos) {
 	size++;
 	int* buffer = new int[size];
@@ -103,6 +222,37 @@ void Insert(int*& array, int& size, int& value, int& pos) {
 	delete[] array;
 	array = buffer;
 	buffer = nullptr;
+}
+void Insert_Row(int**& array, int& rows, const int& cols, const int& pos) {
+	int** buffer = new int* [rows + 1];
+	for (int i = 0; i < rows + 1; i++)
+	{
+		buffer[i] = new int[cols];
+	}
+	for (int i = 0; i < pos; i++)
+	{
+		for (int j = 0; j < cols; j++) {
+			buffer[i][j] = array[i][j];
+		}
+	}
+	for (int i = pos + 1, k = pos; i < rows + 1; i++, k++)
+	{
+		for (int j = 0; j < cols; j++) {
+			buffer[i][j] = array[k][j];
+		}
+	}
+	for (int i = 0; i < rows; i++)
+	{
+		delete[] array[i];
+	}
+	delete[] array;
+	array = buffer;
+	buffer = nullptr;
+	for (int i = 0; i < cols; i++)
+	{
+		array[pos][i] = 0;
+	}
+	rows++;
 }
 void Pop_Back(int*& array, int& size) {
 	size--;
